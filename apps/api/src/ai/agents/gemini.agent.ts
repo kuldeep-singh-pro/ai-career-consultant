8feaca1634d29044d3e29aff04944138bbdf370c
skepttalk <kuldeepsingh861915@gmail.com>
2026-03-26 13:29:43 +0530

    feat:resume model prompt parsing structure working data get

--- apps/api/src/ai/agents/gemini.agent.ts
+++ apps/api/src/ai/agents/gemini.agent.ts
@@ -1,8 +1,19 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { env } from "../../config/env";
 
+console.log("Gemini model:", env.GEMINI_MODEL);
+console.log("Gemini API key exists:", !!env.GEMINI_API_KEY);
+
+if (!env.GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY is missing in environment variables");
+}
+
+if (!env.GEMINI_MODEL) {
+  throw new Error("GEMINI_MODEL is missing in environment variables");
+}
+
 export const geminiModel = new ChatGoogleGenerativeAI({
   model: env.GEMINI_MODEL,
   apiKey: env.GEMINI_API_KEY,
-  temperature: 0.3
+  temperature: 0.3,
 });
--- apps/api/src/dto/resume.dto.ts
+++ apps/api/src/dto/resume.dto.ts
@@ -1,52 +1,25 @@
 import z from "zod";
 
 
-export const UploadResumeDto = z.object({});
+export const UploadResumeDto = z.object({
+  resume: z.any()
+});
+
+export const ExperienceLevelEnum = z.enum([
+  "Beginner",
+  "Intermediate",
+  "Advanced",
+]);
 
 
 export const ResumeAnalysisResponseDto = z.object({
-
   score: z.number().min(0).max(100),
 
   skills: z.array(z.string()),
 
-  experienceLevel: z.enum([
-    "Beginner",
-    "Intermediate",
-    "Advanced"
-  ]),
+  missingSkills: z.array(z.string()),
 
-  suggestions: z.array(z.string()),
-
-  recommendedRoles: z.array(z.string()),
-
-  strengths: z.array(z.string()),
-
-  weaknesses: z.array(z.string())
-
-});
-
-
-export const CreateResumeInternalDto = z.object({
-
-  userId: z.string(),
-
-  fileUrl: z.string(),
-
-  extractedText: z.string()
-
-});
-
-
-export const CreateResumeAnalysisDto = z.object({
-
-  userId: z.string(),
-
-  resumeId: z.string(),
-
-  score: z.number(),
-
-  skills: z.array(z.string()),
+  experienceLevel: ExperienceLevelEnum,
 
   suggestions: z.array(z.string()),
 
@@ -56,6 +29,39 @@ export const CreateResumeAnalysisDto = z.object({
 
   weaknesses: z.array(z.string()),
 
-  experienceLevel: z.string()
+  careerRoadmap: z.array(z.string()),
+});
 
+
+export const CreateResumeInternalDto = z.object({
+  userId: z.string(),
+
+  fileUrl: z.string(),
+
+  extractedText: z.string(),
+});
+
+
+export const CreateResumeAnalysisDto = z.object({
+  userId: z.string(),
+
+  resumeId: z.string(),
+
+  score: z.number(),
+
+  skills: z.array(z.string()),
+
+  missingSkills: z.array(z.string()),
+
+  suggestions: z.array(z.string()),
+
+  recommendedRoles: z.array(z.string()),
+
+  strengths: z.array(z.string()),
+
+  weaknesses: z.array(z.string()),
+
+  careerRoadmap: z.array(z.string()),
+
+  experienceLevel: ExperienceLevelEnum,
 });
